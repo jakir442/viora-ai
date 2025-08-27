2cac275845d06b22449dc91a139336e936981ea4
jakir442 <jakigeming2021@gmail.com>
2025-08-27 22:47:21 +0700

    14: ai agents

--- packages/backend/convex/public/conversations.ts
+++ packages/backend/convex/public/conversations.ts
@@ -1,5 +1,8 @@
 import { mutation, query } from "../_generated/server";
 import { ConvexError, v } from "convex/values";
+import { supportAgent } from "../system/ai/agents/supportAgent";
+import { saveMessage } from "@convex-dev/agent";
+import { components } from "../_generated/api";
 
 export const getOne = query({
   args: {
@@ -12,14 +15,24 @@ export const getOne = query({
     if (!session || session.expiresAt < Date.now()) {
       throw new ConvexError({
         code: "UNAUTHORIZED",
-        message: "Invalid session",
+        message: "Sesi tidak valid",
       });
     }
 
     const conversation = await ctx.db.get(args.conversationId);
 
     if (!conversation) {
-      return null;
+      throw new ConvexError({
+        code: "NOT_FOUND",
+        message: "Percakapan tidak ditemukan",
+      });
+    }
+
+    if (conversation.contactSessionId !== session._id) {
+      throw new ConvexError({
+        code: "UNAUTHORIZED",
+        message: "Sesi salah",
+      });
     }
 
     return {
@@ -45,8 +58,18 @@ export const create = mutation({
       });
     }
 
-    // TODO: Replace once functionality for thread creation is present
-    const threadId = "123";
+    const { threadId } = await supportAgent.createThread(ctx, {
+      userId: args.organizationId,
+    });
+
+    await saveMessage(ctx, components.agent, {
+      threadId,
+      message: {
+        role: "assistant",
+        // TODO: Later modify to widget settings' initial message
+        content: "Halo, bagaimana saya bisa membantu Anda hari ini?",
+      },
+    });
 
     const conversationId = await ctx.db.insert("conversations", {
       contactSessionId: session._id,
---
+++ packages/backend/convex/system/index.ts
@@ -0,0 +1,2 @@
+export * as contactSessions from "./contactSessions";
+export * as conversations from "./conversations";
