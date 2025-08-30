bebd9669b03717e964412fefaaefd372561e76b6
jakir442 <jakigeming2021@gmail.com>
2025-08-30 18:51:09 +0700

    22: ai search tool

--- packages/backend/convex/system/ai/agents/supportAgent.ts
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -1,9 +1,10 @@
 import { google } from "@ai-sdk/google";
 import { Agent } from "@convex-dev/agent";
 import { components } from "../../../_generated/api";
+import { SUPPORT_AGENT_PROMPT } from "../constants";
 
 export const supportAgent = new Agent(components.agent, {
   name: "viora-ai-agent",
   languageModel: google("gemini-2.5-flash"),
-  instructions: `Anda adalah agen dukungan pelanggan. Gunakan alat "resolveConversation" ketika pengguna menyatakan percakapan telah selesai. Gunakan alat "escalateConversation" ketika pengguna menunjukkan rasa frustrasi, atau secara eksplisit meminta bantuan manusia.`,
+  instructions: SUPPORT_AGENT_PROMPT,
 });
